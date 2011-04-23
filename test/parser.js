bb7c0104bbaee05033c04f4063a876ddebc8dea3
Evan Goer <evan@goer.org>
2011-04-23 13:30:22 -0700

    Added basic token stream

--- test/parser.js
+++ test/parser.js
@@ -8,7 +8,7 @@ vows.describe("Parser Tests").addBatch({
         
         "that responds to define() and getToken()": function(symbols) {
             assert.isFunction(symbols.define);
-            assert.isFunction(symbols.getToken);
+            assert.isFunction(symbols.get_token);
         },
         "that can define a fake bold symbol": function(symbols) {
             symbols.define({id:"**"});
@@ -19,13 +19,25 @@ vows.describe("Parser Tests").addBatch({
             assert.equal(symbols.table["(string)"].id, "(string)");
         },
         "that can get a bold token": function(symbols) {
-            var t_bold = symbols.getToken({id: "**"});
+            var t_bold = symbols.get_token({id: "**"});
             assert.equal(t_bold.id, "**");
         },
         "that can get a string token": function(symbols) {
-            var t_string = symbols.getToken({id: "(string)", value: "HELLOSKI"});
+            var t_string = symbols.get_token({id: "(string)", value: "HELLOSKI"});
             assert.equal(t_string.id, "(string)");
             assert.equal(t_string.value, "HELLOSKI");
+        }
+    },
+    "A parser has a token stream" : {
+        topic: parser.token_stream,
+        
+        "that can set a stream of tokens": function(stream) {
+            assert.isFunction(stream.set);
+            stream.set([{id:"(string)", value:"This is "}, {id:"**"}, {id:"(string)", value:"bold"}, {id:"**"}, {id:"(string)", value:" text"}]);
+        },
+        "that has a first token that is a string": function(stream) {
+            var tok = stream.advance();
+            assert.equal(tok.id, "(string)");
         }
     }
 }).export(module);
