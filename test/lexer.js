b4208db19cdb3eb3425038f8639110a66f05fa45
Evan Goer <evan@goer.org>
2011-04-26 22:12:26 -0700

    Added tests for string_token() and lex().

--- test/lexer.js
+++ test/lexer.js
@@ -84,6 +84,108 @@ vows.describe("Lexer Tests").addBatch({
             assert.equal(token.id, "(indent)");
             assert.equal(token.value, 9);
         }
+    }, 
+    
+    "The string_token function": {
+        topic: lexer,
+        
+        "converts a string token at a line's start": function(lexer) {
+            var token = lexer.string_token("0123456789", 0, {index:7});
+            assert.equal(token.id, "(string)");
+            assert.equal(token.value, "0123456");
+        },
+        "trims leading whitespace when at a line's start": function(lexer) {
+            var token = lexer.string_token("  23456789", 0, {index:7});
+            assert.equal(token.id, "(string)");
+            assert.equal(token.value, "23456");
+        },
+        "converts a string token from a line's middle": function(lexer) {
+            var token = lexer.string_token("  23456789", 4, {index:7});
+            assert.equal(token.id, "(string)");
+            assert.equal(token.value, "456");
+        },
+        "converts a string token from a line's end": function(lexer) {
+            var token = lexer.string_token("  23456789", 4);
+            assert.equal(token.id, "(string)");
+            assert.equal(token.value, "456789");
+        },
+        "returns null rather than an empty string token": function(lexer) {
+            assert.equal(lexer.string_token("", 0, null));
+        }
+    },
+    
+    "The lex function": {
+        topic: lexer,
+        
+        "returns an empty array for falsy input": function(lexer) {
+            assert.deepEqual(lexer.lex(null), []);
+        },
+        "returns an indent token for an empty string": function(lexer) {
+            var tokens = lexer.lex("");
+            assert.deepEqual(tokens[0], {id: "(indent)", value: 0});
+        },
+        "returns two indent tokens for two empty lines": function(lexer) {
+            var tokens = lexer.lex("\r\n"),
+                ind = {id: "(indent)", value: 0},
+                expected = [ind, ind];
+            assert.deepEqual(tokens, expected);
+        },
+        "returns IND,STR,BOL,STR for bold text": function(lexer) {
+            var tokens = lexer.lex("**a**"),
+                ind = {id: "(indent)", value: 0},
+                str = {id: "(string)", value: "a"},
+                bol = {id: "**", value: "**"},
+                expected = [ind, bol, str, bol];   
+            assert.deepEqual(tokens, expected);
+        },
+        "returns IND,STR,ITA,STR for italic text": function(lexer) {
+            var tokens = lexer.lex("*a*"),
+                ind = {id: "(indent)", value: 0},
+                str = {id: "(string)", value: "a"},
+                bol = {id: "*", value: "*"},
+                expected = [ind, bol, str, bol];   
+            assert.deepEqual(tokens, expected);
+        }
     }
+    
+    /*
+    1. input.split (2 lines or 1)
+    2. input is falsy
+    3. at least one bold match
+    4. at least one italic match
+    5. string match only
+    
+    var pattern = /(\*\*|\*(?!\*))/g
+
+    function lex(input) {
+        var lines = input ? input.split(/\r?\n/) : {length:0},
+            tokens = [],
+            matches,
+            last_index,
+            str_token;
+
+        for (var i = 0; i < lines.length; i += 1) {
+            last_index = 0;
+            tokens.push(indent_token(lines[i]));
+
+            while ((matches = pattern.exec(lines[i])) !== null) {
+                str_token = string_token(lines[i], last_index, matches);
+                if (str_token) {
+                    tokens.push(str_token);
+                }
+                // Save the index to start the next match for the next iteration,
+                // as the start point for slicing out the next string token.
+                last_index = pattern.lastIndex;
+
+                tokens.push(inline_token(matches));
+            }
+            // after we are out of matches, get the last 
+            tokens.push(string_token(lines[i], last_index));
+        }
+        return tokens;
+    }
+    */
+    
+    
     
 }).export(module);
